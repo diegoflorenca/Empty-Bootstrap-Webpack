0321d0ea2df0645d3e6396195aa67a2b708bf2e9
Diego Felipe Florença <diegoflorenca@gmail.com>
2020-08-30 19:15:47 -0300

    Implement modal example

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,12 @@
 import 'bootstrap';
 import '../css/styles.scss';
+
+const elModalOpen = document.querySelector('#modal-open');
+elModalOpen.addEventListener('click', () => {
+  $('#my-modal').modal('show');
+});
+
+const elModalClose = document.querySelector('#modal-close');
+elModalClose.addEventListener('click', () => {
+  $('#my-modal').modal('hide');
+});
